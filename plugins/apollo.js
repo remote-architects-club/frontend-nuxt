3eb98a072812036e988d2a007d0bdaaa90faad7e
Daniel da Rocha <danrocha@gmail.com>
2020-03-21 08:12:14 +0100

    hotfix:update Apollo URI

--- plugins/apollo.js
+++ plugins/apollo.js
@@ -2,6 +2,8 @@ import ApolloClient from 'apollo-boost'
 import fetch from 'node-fetch'
 
 export const client = new ApolloClient({
-  uri: process.env.GRAPHQL_URI,
+  uri:
+    process.env.GRAPHQL_URI ||
+    'https://remotearchitectsclub-stg.herokuapp.com/v1/graphql',
   fetch
 })
