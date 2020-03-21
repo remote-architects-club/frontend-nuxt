a1cb3c5073f01979361372487ba7baf6887338e8
Daniel da Rocha <danrocha@gmail.com>
2020-03-21 15:22:54 +0100

    add tools and articles pages

---
+++ pages/articles.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1 class="mb-4 text-3xl font-bold">Articles</h1>
+    <div class="p-8 bg-white shadow-lg content">
+      <p>To be done...</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'pages'
+}
+</script>
