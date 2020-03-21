d4f5f2000eb70b87922dcdb397f5b6239bb31a53
Daniel da Rocha <danrocha@gmail.com>
2020-03-21 14:16:45 +0100

    implement about page

---
+++ pages/about.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <h1 class="mb-4 text-3xl font-bold">About</h1>
+    <div v-html="content" class="p-8 bg-white shadow-lg content"></div>
+  </div>
+</template>
+
+<script>
+import gql from 'graphql-tag'
+import { client } from '@/plugins/apollo'
+export default {
+  layout: 'pages',
+  async asyncData() {
+    const { data } = await client.query({
+      query: gql`
+        query about {
+          about {
+            content(markdown: true)
+          }
+        }
+      `
+    })
+    return { content: data.about.content }
+  },
+  data() {
+    return {
+      content: ''
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.content {
+  p {
+    @apply mb-4;
+  }
+  h3 {
+    @apply font-bold mt-8 mb-4 text-lg;
+  }
+  a {
+    @apply underline;
+  }
+  a:hover,
+  a:focus {
+    @apply font-bold;
+  }
+}
+</style>
