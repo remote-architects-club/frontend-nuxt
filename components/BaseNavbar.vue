9f5ad3c51eda858704d5b425a9cd2b4b5e284416
Daniel da Rocha <danrocha@gmail.com>
2020-03-21 15:22:54 +0100

    add tools and articles pages

--- components/BaseNavbar.vue
+++ components/BaseNavbar.vue
@@ -2,6 +2,15 @@
   <nav class="flex justify-end pt-4 mb-4">
     <ul class="flex items-center">
       <li class="font-semibold">
+        <nuxt-link to="/" class="link">home</nuxt-link>
+      </li>
+      <li class="font-semibold">
+        <nuxt-link to="/tools" class="link">tools</nuxt-link>
+      </li>
+      <li class="font-semibold">
+        <nuxt-link to="/articles" class="link">articles</nuxt-link>
+      </li>
+      <li class="font-semibold">
         <nuxt-link to="/about" class="link">about</nuxt-link>
       </li>
     </ul>
@@ -12,4 +21,10 @@
 export default {}
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+ul {
+  li {
+    @apply ml-2;
+  }
+}
+</style>
