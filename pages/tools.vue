f3307c8e06df22b0ef6331d2b4c58b07f9399ee9
Daniel da Rocha <danrocha@gmail.com>
2020-03-22 12:09:23 +0100

    tools: do not show if user added no url

--- pages/tools.vue
+++ pages/tools.vue
@@ -7,50 +7,55 @@
       </div>
     </template>
     <template v-else-if="state.matches('idle') && tools.length > 0">
-      <div
-        v-for="tool in toolsByUsage"
-        :key="tool.id"
-        class="flex p-4 mb-8 bg-white shadow-lg "
-      >
-        <div class="flex-none mr-4 ">
-          <tool-logo :url="tool.url" :name="tool.name" />
-        </div>
-        <div>
-          <div class="flex justify-between">
-            <div class="mb-2">
-              <p class="font-semibold">
-                <!-- <a :href="tool.url" class="link">{{ tool.name }}</a> -->
-                {{ tool.name }}
-              </p>
-              <p v-if="tool.url"><company-url :url="tool.url" /></p>
-            </div>
-            <div>
-              <div class="flex text-xs">
-                <p class="px-2 mr-1 text-xs border rounded-full">
-                  <span v-if="tool.is_free">free</span><span v-else>paid</span>
-                </p>
-
-                <ul
-                  class="flex justify-end"
-                  v-if="tool.tool_categories && tool.tool_categories.length > 0"
-                >
-                  <li
-                    v-for="{ category_name: cat } in tool.tool_categories"
-                    :key="cat"
-                    class="px-2 ml-1 text-xs bg-gray-300 rounded-full"
-                  >
-                    {{ cat }}
-                  </li>
-                </ul>
-              </div>
-              <p v-if="tool.office_tools" class="text-xs text-right">
-                used by {{ tool.office_tools.length }} companies
-              </p>
-            </div>
+      <template v-for="tool in toolsByUsage">
+        <div
+          v-if="tool.url"
+          :key="tool.id"
+          class="flex p-4 mb-8 bg-white shadow-lg "
+        >
+          <div class="flex-none mr-4 ">
+            <tool-logo :url="tool.url" :name="tool.name" />
           </div>
-          <p>{{ tool.description }}</p>
+          <div>
+            <div class="flex justify-between">
+              <div class="mb-2">
+                <p class="font-semibold">
+                  <!-- <a :href="tool.url" class="link">{{ tool.name }}</a> -->
+                  {{ tool.name }}
+                </p>
+                <p v-if="tool.url"><company-url :url="tool.url" /></p>
+              </div>
+              <div>
+                <div class="flex text-xs">
+                  <p class="px-2 mr-1 text-xs border rounded-full">
+                    <span v-if="tool.is_free">free</span
+                    ><span v-else>paid</span>
+                  </p>
+
+                  <ul
+                    class="flex justify-end"
+                    v-if="
+                      tool.tool_categories && tool.tool_categories.length > 0
+                    "
+                  >
+                    <li
+                      v-for="{ category_name: cat } in tool.tool_categories"
+                      :key="cat"
+                      class="px-2 ml-1 text-xs bg-gray-300 rounded-full"
+                    >
+                      {{ cat }}
+                    </li>
+                  </ul>
+                </div>
+                <p v-if="tool.office_tools" class="text-xs text-right">
+                  used by {{ tool.office_tools.length }} companies
+                </p>
+              </div>
+            </div>
+            <p>{{ tool.description }}</p>
+          </div>
         </div>
-      </div>
+      </template>
     </template>
     <template v-else-if="state.matches('idle') && tools.length === 0">
       <p>
