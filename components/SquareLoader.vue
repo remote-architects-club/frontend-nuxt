0401857ec124c1719fbc881ab9343515d106ca68
Daniel da Rocha <danrocha@gmail.com>
2020-03-22 14:43:40 +0100

    move spinner styles to main stylesheet to avoid purgecss

---
+++ components/SquareLoader.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="v-spinner" v-show="loading">
+    <div class="v-square" v-bind:style="spinnerStyle"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SquareLoader',
+
+  props: {
+    loading: {
+      type: Boolean,
+      default: true
+    },
+    color: {
+      type: String,
+      default: '#000'
+    },
+    size: {
+      type: String,
+      default: '50px'
+    }
+  },
+  data() {
+    return {
+      spinnerStyle: {
+        backgroundColor: this.color,
+        height: this.size,
+        width: this.size
+      }
+    }
+  }
+}
+</script>
+
+<style></style>
