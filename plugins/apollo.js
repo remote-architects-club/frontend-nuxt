6acc49857ffbfdf86033ff5b332fc573404ee1bc
Daniel da Rocha <danrocha@gmail.com>
2020-03-22 09:44:35 +0100

    apollo: use prod api

--- plugins/apollo.js
+++ plugins/apollo.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch'
 
 export const client = new ApolloClient({
   uri:
-    process.env.GRAPHQL_URI ||
-    'https://remotearchitectsclub-stg.herokuapp.com/v1/graphql',
+    // process.env.GRAPHQL_URI ||
+    'https://remotearchitectsclub-api.herokuapp.com/v1/graphql',
   fetch
 })
