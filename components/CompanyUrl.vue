3950b832bcb7f9720e0081ace56a381dce031806
Daniel da Rocha <danrocha@gmail.com>
2020-03-22 08:39:56 +0100

    improve style

--- components/CompanyUrl.vue
+++ components/CompanyUrl.vue
@@ -1,6 +1,7 @@
 <template>
   <p class="flex items-center">
-    <v-icon icon="link" class="w-4 h-4" /> {{ domain }}
+    <v-icon icon="link" class="w-4 h-4 mt-1 mr-1" />
+    <a :href="url" class="link">{{ domain }}</a>
   </p>
 </template>
 
