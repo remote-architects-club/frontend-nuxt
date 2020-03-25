933c0ab4b9b1f6c40541bb740d9660d504116051
Daniel da Rocha <danrocha@gmail.com>
2020-03-25 11:23:31 +0100

    cypress: admin speck

---
+++ cypress/integration/admin.spec.js
@@ -0,0 +1,13 @@
+describe('Admin', function() {
+  it('If user not logged in, goes to login page', function() {
+    cy.visit('/admin')
+    cy.url().should('include', '/admin/login')
+    cy.contains('click to login')
+    cy.visit('/admin/tools')
+    cy.url().should('include', '/admin/login')
+    cy.contains('click to login')
+    cy.visit('/admin/articles')
+    cy.url().should('include', '/admin/login')
+    cy.contains('click to login')
+  })
+})
