e965dddd25c332a768226d9656debf0c773f91c3
Daniel <danrocha@gmail.com>
2020-03-28 17:31:12 +0100

    update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,48 @@
 module.exports = {
   root: true,
   env: {
-    browser: true,
-    node: true
+    node: true,
+    browser: true
+  },
+  extends: [
+    'plugin:cypress/recommended',
+    'plugin:vue/recommended',
+    'eslint:recommended',
+    'prettier/vue',
+    'plugin:prettier/recommended'
+  ],
+  rules: {
+    'vue/component-name-in-template-casing': ['error', 'kebab-case'],
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+  },
+  globals: {
+    $nuxt: true
   },
   parserOptions: {
     parser: 'babel-eslint'
-  },
-  extends: [
-    '@nuxtjs',
-    'prettier',
-    'prettier/vue',
-    'plugin:prettier/recommended',
-    'plugin:nuxt/recommended',
-    'plugin:cypress/recommended'
-  ],
-  plugins: ['prettier'],
-  // add your custom rules here
-  rules: {}
+  }
 }
+// module.exports = {
+//   root: true,
+//   env: {
+//     browser: true,
+//     node: true
+//   },
+//   parserOptions: {
+//     parser: 'babel-eslint'
+//   },
+//   extends: [
+//     // 'eslint:recommended',
+//     '@nuxtjs',
+//     'prettier',
+//     'prettier/vue',
+//     'plugin:vue/strongly-recommended',
+//     'plugin:prettier/recommended',
+//     'plugin:nuxt/recommended',
+//     'plugin:cypress/recommended'
+//   ],
+//   plugins: ['prettier', 'vue'],
+//   // add your custom rules here
+//   rules: {}
+// }
