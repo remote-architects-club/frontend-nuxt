b25141ed4fa07f4b12e0c8313e1c6b272f12e9dd
Daniel <danrocha@gmail.com>
2020-03-31 09:54:10 +0200

    cy: implement stubs on tests

--- cypress/integration/contribute.spec.js
+++ cypress/integration/contribute.spec.js
@@ -2,6 +2,25 @@
 // import schema from '../fixtures/schema.graphql'
 
 describe('Contribute', function() {
+  const url = 'https://remotearchitectsclub-stg.herokuapp.com/v1/graphql'
+  beforeEach(() => {
+    cy.server({
+      method: 'POST',
+      delay: 800,
+      headers: {
+        'content-type': 'application/json; charset=utf-8'
+      }
+    })
+    cy.route({
+      url: 'https://api.ipify.org/?format=json',
+      method: 'GET',
+      status: 200,
+      response: {
+        ip: '95.90.239.239'
+      }
+    })
+    cy.fixture('search_offices').as('search_offices')
+  })
   it('Goes to first step of contributing process when clicking on contribute', function() {
     cy.visit('/')
     cy.contains('contribute').click()
@@ -9,12 +28,32 @@ describe('Contribute', function() {
     cy.get('[data-cy=office-name]')
   })
   it('Adds new company', function() {
-    const name = 'My New Company'
+    const name =
+      Math.random()
+        .toString(36)
+        .substring(2, 15) +
+      Math.random()
+        .toString(36)
+        .substring(2, 15)
     const city = 'Berlin'
     const website = 'http://example.com'
+    const country = 'ZW'
     // search page
+    cy.fixture('countries').then((results) => {
+      cy.route({
+        url,
+        status: 200,
+        response: results
+      })
+    })
     cy.visit('/add')
     cy.url().should('include', '/add')
+    // search offices results will be empty
+    cy.route({
+      url,
+      status: 200,
+      response: { data: { search_offices: [] } }
+    })
     cy.get('[data-cy=office-name]')
       .type(name)
       .should('have.value', name)
@@ -70,47 +109,83 @@ describe('Contribute', function() {
       .clear()
       .type(name)
     cy.get('[data-cy=error-name]').should('not.be.visible')
-    // cy.get('@save').click()
+    cy.fixture('insert_office').then((results) => {
+      results.data.insert_office.returning[0].name = name
+      results.data.insert_office.returning[0].url = website
+      results.data.insert_office.returning[0].city = city
+      results.data.insert_office.returning[0].country_iso = country
+      cy.route({
+        url,
+        status: 200,
+        response: results
+      })
+      cy.get('@save').click()
+      cy.url().should(
+        'include',
+        `/add/company?id=${results.data.insert_office.returning[0].id}`
+      )
+    })
+    cy.contains(name)
+    cy.contains(city)
+    cy.contains(country)
+    cy.contains(website)
     // cancel
-    cy.get('@cancel').click()
-    cy.get('@add-form').should('not.exist')
-    cy.get('[data-cy=office-name]')
+    // cy.get('@cancel').click()
+    // cy.get('@add-form').should('not.exist')
+    // cy.get('[data-cy=office-name]')
   })
   it('Searches and finds and uses an existing company', function() {
+    cy.fixture('countries').then((results) => {
+      cy.route({
+        url,
+        status: 200,
+        response: results
+      })
+    })
     cy.visit('/add')
     cy.url().should('include', '/add')
+    cy.fixture('search_offices').then((results) => {
+      cy.route({
+        url,
+        status: 200,
+        response: results
+      })
+    })
     cy.get('[data-cy=office-name]')
       .type('henn')
       .should('have.value', 'henn')
 
     cy.get('[data-cy=results]').contains('HENN')
+    cy.fixture('office_by_pk').then((results) => {
+      cy.route({
+        url,
+        status: 200,
+        response: results
+      })
+    })
     cy.get('[data-cy=results]')
       .contains('select')
       .first()
       .click()
-
-    cy.url().should('include', '/add/')
-    cy.contains('HENN')
+    cy.fixture('office_by_pk').then((results) => {
+      cy.url().should(
+        'include',
+        `/add/company?id=${results.data.office_by_pk.id}`
+      )
+      cy.contains(`${results.data.office_by_pk.name}`)
+    })
     cy.contains('All good, continue').click()
+    cy.fixture('office_by_pk').then((results) => {
+      cy.url().should(
+        'include',
+        `/add/personal?id=${results.data.office_by_pk.id}`
+      )
+    })
   })
-  it('Goes through personal flow wfh01', function() {
-    cy.visit('/add')
-    cy.url().should('include', '/add')
-    cy.get('[data-cy=office-name]')
-      .type('henn')
-      .should('have.value', 'henn')
-
-    cy.get('[data-cy=results]').contains('HENN')
-    cy.get('[data-cy=results]')
-      .contains('select')
-      .first()
-      .click()
-
-    cy.url().should('include', '/add/')
-    cy.contains('HENN')
-    cy.contains('All good, continue').click()
-    // personal form
-    cy.url().should('include', '/personal')
+  it.only('Goes through personal flow wfh01', function() {
+    cy.fixture('office_by_pk').then((results) => {
+      cy.visit(`/add/personal?id=${results.data.office_by_pk.id}`)
+    })
     // name
     cy.get('[data-cy=previous]')
       .as('previous')
@@ -191,6 +266,13 @@ describe('Contribute', function() {
   it('Goes through personal flow wfh02', function() {
     cy.visit('/add')
     cy.url().should('include', '/add')
+    cy.fixture('search_offices').then((results) => {
+      cy.route({
+        url,
+        status: 200,
+        response: results
+      })
+    })
     cy.get('[data-cy=office-name]')
       .type('henn')
       .should('have.value', 'henn')
@@ -275,9 +357,24 @@ describe('Contribute', function() {
     cy.get('[data-cy=final_tips]')
     // cy.get('@next').click()
   })
-  it.only('Goes through personal flow NOT wfh01', function() {
+  it('Goes through personal flow NOT wfh01', function() {
+    cy.fixture('countries').then((results) => {
+      cy.route({
+        url,
+        status: 200,
+        response: results
+      })
+    })
     cy.visit('/add')
     cy.url().should('include', '/add')
+    cy.fixture('search_offices').then((results) => {
+      cy.route({
+        url,
+        status: 200,
+        response: results
+      })
+    })
+
     cy.get('[data-cy=office-name]')
       .type('henn')
       .should('have.value', 'henn')
@@ -287,12 +384,26 @@ describe('Contribute', function() {
       .contains('select')
       .first()
       .click()
-
-    cy.url().should('include', '/add/')
+    cy.fixture('office_by_pk').then((results) => {
+      cy.route({
+        url,
+        status: 200,
+        response: results
+      })
+    })
+    cy.route({
+      url: 'https://api.ipify.org/?format=json',
+      method: 'GET',
+      status: 200,
+      response: {
+        ip: '95.90.239.239'
+      }
+    })
+    cy.url().should('include', `/add/company?id=`)
     cy.contains('HENN')
     cy.contains('All good, continue').click()
     // personal form
-    cy.url().should('include', '/personal')
+    cy.url().should('include', '/personal?id=')
     // name
     cy.get('[data-cy=previous]')
       .as('previous')
@@ -353,6 +464,14 @@ describe('Contribute', function() {
       .contains('finish')
       .should('be.enabled')
     cy.get('[data-cy=final_tips]')
-    // cy.get('@next').click()
+    cy.fixture('insert_experience').then((results) => {
+      cy.route({
+        url,
+        status: 200,
+        response: results
+      })
+    })
+    cy.get('@next').click()
+    cy.contains('Thank you!')
   })
 })
