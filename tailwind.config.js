1a8ebf19be477352c2b2a33e4b7b26aefc73f93a
Daniel da Rocha <danrocha@gmail.com>
2020-08-01 12:45:23 +0200

    fix tailwind purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,4 +25,14 @@ module.exports = {
     cursor: ['responsive', 'disabled'],
   },
   plugins: [require('@tailwindcss/typography')],
+  purge: {
+    enabled: process.env.NODE_ENV === 'production',
+    content: [
+      'components/**/*.vue',
+      'layouts/**/*.vue',
+      'pages/**/*.vue',
+      'plugins/**/*.js',
+      'nuxt.config.js',
+    ],
+  },
 }
